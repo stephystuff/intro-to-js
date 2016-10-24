7d7326c582f2798320e696a3ad5daf8cbc494ff0
StephyStuff <stephanie.birch13@gmail.com>
2016-10-24 13:17:21 -0400

    final version

--- js/code.js
+++ js/code.js
@@ -17,18 +17,25 @@
      // your first name.
      // Also DECLARE (but do not instantiate) a `lastName` variable.
 
-     
+     var firstName = 'Stephanie';
+     var lastName;
+
+
+
 
      // TODO 2: Create a `greeting` variable as a string which says:
      //    "Hello there _____!"
      // Replace the _____ above with the value of the `firstName` variable.
      // NOTE: Do NOT simply retype your first name in the `greeting` value.
 
-
+     var greeting = 'Hello there ' + firstName + '!';
 
      // TODO 3: Create two variables: one for the year you were born, and one
      // for the current year. Then create a third variable to store your age!
      // Name these: `birthYear`, `currentYear`, and `age`
+     var birthYear = 1985;
+     var currentYear = 2016;
+     var age = 31;
 
 
 
@@ -36,7 +43,13 @@
      // store "hip" in this variable, but if your age is 35 or over, store
      // "classic" in this variable.
 
+    var style = 'hip'
 
+     if (age < 35) {
+     console.log ('hip');
+   } else {
+     console.log ('classic');
+   }
 
      // TODO 5: Create a variable indicating that your instructor is awesome
      // and call it `instructorIsAwesome`. This variable should be a boolean
@@ -44,18 +57,27 @@
      // Next, create a variable called `review` and, if the instructor is
      // awesome, set `review` to "good", otherwise set the `review` variable to "bad"
 
+     var instructorIsAwesome = true;
+     var review = 'good';
 
 
      // TODO 6: Create a function called `math` which accepts to numbers as
      // arguments and returns the result of adding them together, mutliplying
      // by 10, then dividing by the first number.
 
+      function math (x, y) {
+        return (x + y) * 10 / x;
+      }
 
 
      // TODO 7: Write a function called `madlib` that takes 3 separate words as
      // arguments. The function should insert the words into this sentence:
      // "I prefer __1__ while I __2__ so that I don't __3__ on the __4__."
      // Finally, the function should return that new sentence.
+
+     function madlib (action1, action2, action3, action4) {
+       return 'I prefer ' + action1 + ' while I ' + action2 + ' so that I don\'t ' + action3 + ' on the ' + action4 + '.';
+     }
 
 
 
@@ -99,15 +121,15 @@
     }
 
     // Task 1
-	assert(typeof(firstName) === 'string', '`firstName` is not a string (got ' + typeof(firstName) + ' instead)');
-    assert(!window.firstName, '`firstName` was not declared correctly!');
-	assert(typeof(firstName) !== 'undefined' && firstName.length > 0, '`firstName` has no value');
-    try {
-        lastName === '';
-    } catch(err) {
-        assert(false, '`lastName` was not declared');
-    }
-    assert(typeof(lastName) === 'undefined', '`lastName` should not have a value (found a ' + typeof(firstName) + ' instead)');
+    assert(typeof(firstName) === 'string', '`firstName` is not a string (got ' + typeof(firstName) + ' instead)');
+      assert(!window.firstName, '`firstName` was not declared correctly!');
+    assert(typeof(firstName) !== 'undefined' && firstName.length > 0, '`firstName` has no value');
+      try {
+          lastName === '';
+      } catch(err) {
+          assert(false, '`lastName` was not declared');
+      }
+      assert(typeof(lastName) === 'undefined', '`lastName` should not have a value (found a ' + typeof(firstName) + ' instead)');
 
     // Task 2
     assert(typeof(greeting) === 'string', '`greeting` is not a string (got ' + typeof(greeting) + ' instead)');
